b973ba3a1ad620eae16f84fab9cedba659df2801
ultrasukiyaki <37950927+ultrasukiyaki@users.noreply.github.com>
2024-09-24 08:36:00 +0900

    コンフィグからマルチバイト文字抜き忘れてた

--- config.bat
+++ config.bat
@@ -5,15 +5,15 @@ setlocal enabledelayedexpansion
 set myDir=%~dp0
 
 if "%video_dir%"=="" (
-    set /p video_dir=録画ディレクトリを入力してください。最後は\で終わらせてください。:
+    set /p video_dir="Please input Saving video dir, At last input \.:"
 )
 
 if "%stream_key%"=="" (
-    set /p stream_key=YouTubeストリームキーを入力してください。:
+    set /p stream_key="Please input YouTube stream key:"
 )
 
 echo video_dir=%video_dir%> %myDir%\config.txt
 echo stream_key=%stream_key%>> %myDir%\config.txt
 
-echo 設定を保存しました。
+echo "Config was saved."
 pause
